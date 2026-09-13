bf8be75f6d3248a3d96a32393ceb11cc0ffbbebf
Turhan-debug <katanthe030@gmail.com>
2026-09-13 22:46:24 +0600

    Withdraw

--- bankmanegment.py
+++ bankmanegment.py
@@ -16,6 +16,17 @@ def deposit(self, amount):
             print(f"New Balance: {self.balance}")
         else:
             print("Invalid deposit amount.")
+            
+def withdraw(self, amount):
+        if amount <= 0:
+            print("Invalid withdrawal amount.")
+        elif amount > self.balance:
+            print("Insufficient balance.")
+        else:
+            self.balance -= amount
+            print(f"{amount} withdrawn successfully.")
+            print(f"New Balance: {self.balance}")
+
 
         
    
