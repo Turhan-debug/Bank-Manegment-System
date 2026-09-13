9c3ca2006cad1d0d27d8a8a4160afcfd5cc906bb
Turhan-debug <katanthe030@gmail.com>
2026-09-13 22:39:44 +0600

    first commit

---
+++ bankmanegment.py
@@ -0,0 +1,13 @@
+import random
+
+class BankAccount:
+    def __init__(self, account_number, account_holder, balance=0):
+        self.account_number = account_number or random.randint(10000000, 99999999)
+        self.account_holder = account_holder
+        self.balance = balance
+        
+   
+
+
+            
+            
